e64c81a4bf6305da37a157399bbcc4949a222073
bdjm <brendandjmoore@gmail.com>
2021-04-01 11:19:31 +1000

    adding function to be able to save notes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,3 +4,21 @@ let update = function () {
     );
   };
   setInterval(update, 1000);
+
+  $(document).ready(function () {
+    let timeBlocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    function saveNotes() {
+      for (let i = 0; i <= timeBlocks.length; i++) {
+        $("#" + timeBlocks[i]).val(localStorage.getItem(timeBlocks[i]));
+      }
+    }
+    saveNotes();
+  
+    $(".saveBtn").click(function () {
+      let dataHour = $(this).attr("data-hour");
+      let inputField = $("#" + dataHour).val();
+  
+      localStorage.setItem(dataHour, inputField);
+      console.log(inputField);
+    });
+  });
